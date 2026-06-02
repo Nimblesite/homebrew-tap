1901e7c9728bc647c2425dcf9ad16ca15a6b5873
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2026-06-03 07:39:22 +1000

    Update dart_mutant to v0.5.0

--- Formula/dart_mutant.rb
+++ Formula/dart_mutant.rb
@@ -5,19 +5,19 @@ class DartMutant < Formula
   desc "Mutation testing tool for Dart using tree-sitter AST parsing"
   homepage "https://github.com/Nimblesite/dart_mutant"
   license "MIT"
-  version "0.4.0"
+  version "0.5.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/Nimblesite/dart_mutant/releases/download/v0.4.0/dart_mutant-v0.4.0-aarch64-apple-darwin.tar.gz"
-      sha256 "2cb8d6fb20b546284d9be65ada47649e73720544837f35804e6d23c54bbb2005"
+      url "https://github.com/Nimblesite/dart_mutant/releases/download/v0.5.0/dart_mutant-v0.5.0-aarch64-apple-darwin.tar.gz"
+      sha256 "c3e9dbb1c1e478d884ecab2e4efafd4cfb5622c788b1ea6e5f9f1e66f31cc0af"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/Nimblesite/dart_mutant/releases/download/v0.4.0/dart_mutant-v0.4.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "6e4233f8355753e04e8dbb184b643eb6fbd794d8b141e8600199670e2997a0a7"
+      url "https://github.com/Nimblesite/dart_mutant/releases/download/v0.5.0/dart_mutant-v0.5.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "664f606afe32c4acb441cfaa61fc564bd36630aa3da2e27029ea8d547b752055"
     end
   end
 
@@ -26,6 +26,6 @@ class DartMutant < Formula
   end
 
   test do
-    assert_match "dart_mutant", shell_output("\#{bin}/dart_mutant --version")
+    assert_match "dart_mutant", shell_output("#{bin}/dart_mutant --version")
   end
 end
